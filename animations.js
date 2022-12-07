d098f9ccfcdd30a332a0e6e90bff245cc5fecb99
Daniel Miao <danielmiao2019@icloud.com>
2022-12-07 10:38:34 -0800

    upload

---
+++ animations.js
@@ -0,0 +1,142 @@
+setTimeout(function() {
+  var element = document.getElementById("in-animation");
+
+  if (parameters["ie"]) {
+    return element.parentNode.removeChild(element);
+  };
+
+  element.animate([{"opacity": 1}, {"opacity": 0}], {duration: 1000, iterations: 1}).finished.then(function() {
+    element.remove();
+  });
+}, 1000);
+
+function showElement(element, style_display, duration, iterations) {
+  duration = typeof duration !== "undefined" ? duration : 200;
+  iterations = typeof iterations !== "undefined" ? iterations : 1;
+  style_display = typeof style_display !== "undefined" ? style_display : "block";
+
+	element.style.display = style_display;
+	element.animate([{opacity: 0}, {opacity: 1}], {duration: duration, iterations: iterations});
+};
+
+function hideElement(element, style_display, duration, iterations) {
+  duration = typeof duration !== "undefined" ? duration : 200;
+  iterations = typeof iterations !== "undefined" ? iterations : 1;
+  style_display = typeof style_display !== "undefined" ? style_display : "none";
+
+	element.animate([{opacity: 1}, {opacity: 0}], {duration: duration, iterations: iterations}).finished.then(function() {
+		element.style.display = style_display;
+		element.opacity = "1";
+	});
+};
+
+function crossFade(element1, element2) {
+	element1.animate([{opacity: 1}, {opacity: 0}], {duration: 1000, iterations: 1}).finished.then(function() {
+		element1.style.display = "none";
+	});
+	element2.style.display = "initial";
+	element2.animate([{opacity: 0}, {opacity: 1}], {duration: 1000, iterations: 1});
+};
+
+function newChapterAnimation(chapter_number, name) {
+  var element = document.createElement("div");
+  var wrapper = document.createElement("div");
+  wrapper.classList.add("wrapper");
+
+  var part = document.createElement("p");
+  part.innerHTML = "Part " + chapter_number;
+  wrapper.appendChild(part);
+  var chapter_name = document.createElement("p");
+  chapter_name.innerHTML = name;
+  wrapper.appendChild(chapter_name);
+
+  element.appendChild(wrapper);
+
+  element.classList.add("new-chapter");
+  document.body.appendChild(element);
+
+  element.animate([{opacity: "0"}, {opacity: "1"}], {duration: 250, iterations: 1});
+
+  setTimeout(function() {
+    element.animate([{opacity: "1"}, {opacity: "0"}], {duration: 750, iterations: 1}).finished.then(function() {
+      element.remove();
+    });
+  }, 2500);
+};
+
+function preinitiatePage() {
+  updateElementsOnResize();
+  if (parameters["ie"]) {
+    if (document.getElementById("previous-page").classList.contains("white")) {
+      document.getElementById("previous-page").classList.remove("white");
+    };
+
+    if (document.getElementById("next-page").classList.contains("white")) {
+      document.getElementById("next-page").classList.remove("white");
+    };
+  } else {
+    document.getElementById("previous-page").style.setProperty("--border-color", document.documentElement.style.getPropertyValue("--foreground-primary"));
+    document.getElementById("next-page").style.setProperty("--border-color", document.documentElement.style.getPropertyValue("--foreground-primary"));
+  };
+
+  switch (current_section) {
+    case 1:
+      if (parameters["ie"]) {
+        document.getElementById("previous-page").classList.add("white");
+        document.getElementById("next-page").classList.add("white");
+      } else {
+        document.getElementById("previous-page").style.setProperty("--border-color", "white");
+        document.getElementById("next-page").style.setProperty("--border-color", "white");
+      };
+      break;
+    case 2:
+      document.getElementById("ghg1_image").src = "/images/ghg1" + extension;
+      document.getElementById("ghg2_image").src = "/images/ghg2" + extension;
+      break;
+    case 3:
+      newChapterAnimation(2, "Causes");
+      break;
+    case 5:
+      newChapterAnimation(3, "Impacts");
+      break;
+    case 6:
+      newChapterAnimation(4, "Future");
+      break;
+    case 7:
+      if (parameters["ie"]) {
+        document.getElementById("previous-page").classList.add("white");
+        document.getElementById("next-page").classList.add("white");
+      } else {
+        document.getElementById("previous-page").style.setProperty("--border-color", "white");
+        document.getElementById("next-page").style.setProperty("--border-color", "white");
+      };
+      break;
+    case 8:
+      newChapterAnimation(5, "Conclusion");
+      break;
+  };
+}
+
+function initiatePage() {
+  sections.forEach(function(section) {
+    if (section.classList.contains("active")){
+      section.classList.remove("active");
+    };
+  });
+
+  sections[current_section].classList.add("active");
+  switch (current_section) {
+    case 1:
+      sections[1].getElementsByTagName("p")[0].animate([{marginLeft: -Math.floor(window.innerWidth) + "px"}, {marginLeft: "0px"}], {duration: 1000, iterations: 1, easing: "ease-in-out"});
+      break;
+    case 2:
+      sections[2].getElementsByTagName("p")[0].animate([{marginRight: -Math.floor(window.innerWidth) + "px"}, {marginRight: "0px"}], {duration: 1000, iterations: 1, easing: "ease-in-out"});
+      break;
+    case 7:
+      sections[7].getElementsByTagName("p")[0].animate([{marginLeft: -Math.floor(window.innerWidth) + "px"}, {marginLeft: "0px"}], {duration: 1000, iterations: 1, easing: "ease-in-out"});
+      break
+    case 8:
+      sections[8].getElementsByTagName("p")[0].animate([{marginLeft: -Math.floor(window.innerWidth) + "px"}, {marginLeft: "0px"}], {duration: 1000, iterations: 1, easing: "ease-in-out"});
+      break
+  };
+};
---
+++ script.js
@@ -0,0 +1,108 @@
+if (parameters["ie"]) {
+	document.location = "#";
+
+	Element.prototype.classList = {
+    contains: function(string) {
+      return this.className.split(" ").includes(string);
+    },
+    add: function(string) {
+      this.className += " " + string;
+    },
+    remove: function(string) {
+      this.className = this.className.replace(string, "");
+    }
+  }
+
+	function isIE () {
+		var user_agent = navigator.userAgent.toLowerCase();
+		return (user_agent.indexOf("msie") != -1) ? parseInt(user_agent.split("msie")[1]) : false;
+	};
+
+	var browser_warning = document.createElement("p");
+	browser_warning.classList.add("browser-warning");
+
+	switch (isIE()) {
+		case false:
+			browser_warning.innerText = "Internet Explorer 11 is not fully supported. Features such as dark mode and background music are disabled."
+			break;
+		case 10:
+			browser_warning.innerText = "Internet Explorer 10 is not supported. Most features are disabled, and the page may not function properly."
+			break;
+		default:
+			browser_warning.innerText = "Internet Explorer versions below 10 are completely unsupported. The page will not display properly."
+			break;		
+	};
+
+	var close_warning = document.createElement("button");
+	close_warning.classList.add("close-browser-warning");
+	close_warning.innerHTML = "&times;";
+	close_warning.addEventListener("click", function() {
+		var element = document.getElementsByClassName("browser-warning")[0];
+		element.parentNode.removeChild(element);
+	});
+	browser_warning.appendChild(close_warning);
+
+	document.body.appendChild(browser_warning);
+
+	document.getElementById("next-page").classList.remove("uninitiated");
+};
+
+if (parameters["dark_mode"] == "1") {
+	document.documentElement.classList.add("dark");
+};
+
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
+if (document.createElement("canvas").toDataURL("image/webp").indexOf("data:image/webp") == 0) {
+	extension = ".webp";
+} else {
+	extension = ".png";
+};
+
+document.documentElement.style.setProperty("--earth-1-image", "url(/images/earth_1/1080p" + extension + ")");
+document.documentElement.style.setProperty("--earth-2-image", "url(/images/earth_2/1080p" + extension + ")");
+document.documentElement.style.setProperty("--electricity-2-image", "url(/images/electricity_2/1080p" + extension + ")");
+document.documentElement.style.setProperty("--ocean-2-image", "url(/images/ocean_2/1080p" + extension + ")");
+document.documentElement.style.setProperty("--road-1-image", "url(/images/road_1/1080p" + extension + ")");
+document.documentElement.style.setProperty("--solar-1-image", "url(/images/solar_1/1080p" + extension + ")");
+
+var chart_color_choices = ["aqua", "black", "blue", "fuchsia", "gray", "green", "lime", "maroon", "navy", "olive", "purple", "red", "silver", "teal", "white", "yellow"];
+
+document.getElementById("previous-page").style.display = "none";
+
+Array.from(document.getElementsByClassName("previous-image")).forEach(function(element) {
+	element.addEventListener("click", function() {
+		var image_set = this.parentNode.getElementsByClassName("image-set")[0];
+		var selected = parseInt(image_set.dataset.selected);
+		image_set.children[selected].style.display = "none";
+		image_set.children[selected - 1].style.display = "initial";
+		image_set.dataset.selected = selected - 1;
+
+		this.parentNode.getElementsByClassName("next-image")[0].disabled = false;
+
+		if (selected == 1) {
+			this.disabled = true;
+			return;
+		};
+	});
+});
+
+
+Array.from(document.getElementsByClassName("next-image")).forEach(function(element) {
+	element.addEventListener("click", function() {
+		var image_set = this.parentNode.getElementsByClassName("image-set")[0];
+		var selected = parseInt(image_set.dataset.selected);
+		image_set.children[selected].style.display = "none";
+		image_set.children[selected + 1].style.display = "initial";
+		image_set.dataset.selected = selected + 1;
+
+		this.parentNode.getElementsByClassName("previous-image")[0].disabled = false;
+
+		if (selected + 1 >= image_set.children.length - 1) {
+			this.disabled = true;
+			return;
+		};
+	});
+});
